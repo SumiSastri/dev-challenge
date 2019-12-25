0d9eed4af900f37206bd1f6ad01f69ef9cab515e
Your Name <sumisastri@gmail.com>
2019-12-25 22:30:53 +0100

    updates express body-parser to express

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use('/products', require('./routes/products'));
 
 app.get('/', (req, res) => {
